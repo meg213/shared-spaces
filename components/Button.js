54cd0590a8f9d33ca9480afa524a9ee096de40b3
Maxwell Gilbert <maxwellgilbert99@gmail.com>
2021-01-28 14:16:42 -0500

    centered button

--- components/Button.js
+++ components/Button.js
@@ -16,6 +16,7 @@ const styles = StyleSheet.create({
       padding: 12,
       justifyContent: 'center',
       alignItems: 'center',
+      alignSelf: 'center',
       margin: 6,
     },
     text: {

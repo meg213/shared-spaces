f929f945bad95b93ccf43677c197396d13834a9a
hbui36 <hbui36@gatech.edu>
2021-02-28 15:05:57 -0500

    fetch spaces from user collection

--- screens/MySpacesPage.js
+++ screens/MySpacesPage.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { ScrollView, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { Icon } from 'react-native-elements';
 import Button from '../components/Button';
 import SpaceCard from '../components/SpaceCard';
 import ProfilePage from '../screens/ProfilePage';
+// import { createSpaceCard } from '../utils/createSpaceCard';
+import firebase from 'firebase/app';
+import { db } from '../config/keys';
+
+const userRef = db.collection('users');
+const spaceRef = db.collection('spaces');
 
 export default function MySpacesPage({navigation}){
-    return(
+    const currUser = firebase.auth().currentUser;
+    console.log("user: " + currUser.uid);
+    const[spaceNames, setSpaceNames] = useState([])
+
+    const componentIsMounted = useRef(true);
+    useEffect(() => {
+        return () => {
+          componentIsMounted.current = false;
+        };
+      }, []);
+
+
+    useEffect(() => {
+        async function createSpaceCard(currentUser) {
+            var spaces = [];
+            var names = []
+    
+            await userRef.doc(currentUser.uid)
+            .get()
+            .then(documentSnapshot => spaces = documentSnapshot.get('spaces'));
+            
+            for (let i = 0; i < spaces.length; i++) {
+                let spaceData = (await spaceRef.doc(spaces[i].substring(7)).get()).data();
+                names.push({name: spaceData.name});
+            }
+            if (componentIsMounted.current) {
+                setSpaceNames(names)
+            }
+        }
+        createSpaceCard(currUser)
+    }, []);
+
+    console.log(spaceNames);
+
+    return (
         <SafeAreaView style = {[styles.container]}>
             <View>
                 <Text style = {[styles.text]}>
@@ -14,16 +54,10 @@ export default function MySpacesPage({navigation}){
                 </Text>
             </View>
             <ScrollView>
-                <SpaceCard
-                    onClick={() => {
-                        navigation.navigate('SpacePage');
-                    }}
-                />
-                <SpaceCard
-                    onClick={() => {
-                        navigation.navigate('SpacePage');
-                    }}
-                />
+                <div>
+                    {spaceNames.map((name, index) => <SpaceCard key={index} name={name.name}/>
+                    )}
+                </div>
                 <Button
                     name = "Create Space"
                     onClick={() => {
@@ -32,8 +66,9 @@ export default function MySpacesPage({navigation}){
                 />
             </ScrollView>
         </SafeAreaView>
+    )
         
-    );
+    
 }
 
 const styles = StyleSheet.create({

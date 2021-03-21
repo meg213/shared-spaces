2c34d1f722b8b7e8c68f70bcb30036a681bf755e
Jackson Williams <jwilliams01619@gmail.com>
2021-03-21 12:56:09 -0400

    Initial changes to Space page's recently added items

--- screens/SpacePage.js
+++ screens/SpacePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect, useRef, Component} from 'react';
 import { ScrollView, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { Icon } from 'react-native-elements';
 import Item from "../components/Item";
@@ -6,10 +6,64 @@ import User from "../components/User";
 import Card from "../components/Card";
 import Button from "../components/Button";
 import RecentMessageShow from "../components/RecentMessageShow";
+import { db } from '../config/keys';
+import { FlatList } from 'react-native';
+import List from '../components/List';
+import { cos } from 'react-native-reanimated';
+import { AlphabetList } from 'react-native-section-alphabet-list';
+
+const itemRef = db.collection('items');
+const userRef = db.collection('users');
+const spaceRef = db.collection('spaces');
 
 export default function SpacePage({route, navigation}){
-  console.log(route)
   //route params: spaceId, currUser
+
+  const[recentItems, setItems] = useState([]);
+  const componentIsMounted = useRef(true);
+  const currSpaceID = route.params.data.substring(7);
+
+  useEffect(() => {
+    return () => {
+      componentIsMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    const subscriber = spaceRef.doc(currSpaceID).onSnapshot(documentSnapshot => {updateRecentItems(documentSnapshot)});
+    async function updateRecentItems(documentSnapshot) {
+        var space = documentSnapshot.data();
+        var items = space.items.reverse();
+
+        var recent_items_in_order = [];
+
+        for (let i = 0; i < items.length; i++) {
+            let itemData = (await itemRef.doc(items[i].substring(6)).get()).data();
+            let owner = (await userRef.doc(itemData.userID.substring(6)).get()).data().firstname;
+            if (itemData == undefined || owner == undefined) {
+                continue
+            }
+            recent_items_in_order.push({owner: owner, item: itemData});
+        }
+
+        if (componentIsMounted.current) {
+            setItems(recent_items_in_order)
+        }
+    }
+    return () => subscriber;
+  }, []);
+
+  let recent_items_stack = []   // Stack representing the recent items page
+  const max_items_shown = 5;    // How many items we want to show in "Recently Added Items"
+  for (let i = recentItems.length - 1; i > -1; i--) {
+    recent_items_stack.push({key: recentItems[i], value: recentItems[i].item.name})
+    
+    if (recent_items_stack.length > max_items_shown) {
+        recent_items_stack.shift();      // Remove first element of the array, in queue fashion
+        break;                         // Recent items stack is filled, exit loop
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
         <View style ={{
@@ -97,13 +151,19 @@ export default function SpacePage({route, navigation}){
                 }}> 
                     Recently Added Items 
                 </Text>
-                <Item
-                    onClick={()=> {console.log('item click test')}}
+
+                <AlphabetList
+                    data = {recent_items_stack}
+                    renderCustomItem={(item) => (
+                      <Item 
+                        listPage
+                        itemName={item.value}
+                        list={item.key.item.category}
+                        owner={item.key.owner}
+                        shared={item.key.item.isShared}
+                      />
+                    )}
                 />
-                <Item/>
-                <Item/>
-                <Item/>
-                <Item/>
             </View>
         </ScrollView>
     </SafeAreaView>

7c369d917a4677f41dabca426e349a78c7aec38b
Jackson Williams <jwilliams01619@gmail.com>
2021-03-29 10:37:29 -0400

    Added comments to Firebase fuctnoins and getters for Firebase data

--- utils/firebaseMethod.js
+++ utils/firebaseMethod.js
@@ -9,7 +9,7 @@ const itemRef = db.collection("items")
 const listRef = db.collection("lists")
 
 /**
- * 
+ * Add new user to target Space
  * @param targetSpace Space ID new user wishes to join
  * @param requestingUser User requesting to join space
  */
@@ -35,7 +35,10 @@ export async function addNewUser(targetSpace, requestingUser) {
         Alert.alert(e.message);
     }
 }
-
+/**
+ * Deletes user from Firebase
+ * @param currentUser User requesting deletion
+ */
 export async function deleteUser(currentUser) {
     const userID = currentUser.substring(6);
 
@@ -49,6 +52,14 @@ export async function deleteUser(currentUser) {
     }
 }
 
+/**
+ * Creates new Item within SPACE
+ * @param currentUser       User creating the item
+ * @param currentSpaceId    Space item belongs to
+ * @param itemName          Name of the item
+ * @param itemCategory      Type of the item
+ * @param isShared          Is the item shared or not?
+ */
 export async function createItems(currentUser, currentSpaceId, itemName, itemCategory, isShared) {
     try {
         const currItem = itemRef.add({
@@ -56,6 +67,7 @@ export async function createItems(currentUser, currentSpaceId, itemName, itemCat
             isShared: isShared,
             name: itemName,
             spaceID: currentSpaceId,
+            listID: "",
             userID: "users/" + currentUser.uid
         });
         spaceRef.doc(currentSpaceId.substring(7))
@@ -68,6 +80,58 @@ export async function createItems(currentUser, currentSpaceId, itemName, itemCat
     }
 }
 
+/**
+ * Creates new Item within LIST
+ * @param currentUser       User creating the item
+ * @param targetList        List item belongs to
+ * @param itemName          Name of the item
+ * @param itemCategory      Type of the item
+ * @param isShared          Is the item shared or not?
+ */
+ export async function createItemInList(currentUser, targetList, itemName, itemCategory, isShared) {
+    try {
+        const currItem = itemRef.add({
+            category: itemCategory,
+            isShared: isShared,
+            name: itemName,
+            spaceID: "",
+            listID: targetList,
+            userID: "users/" + currentUser.uid
+        });
+
+        listRef.doc(targetList.substring(6)).update({
+            items: firebase.firestore.FieldValue.arrayUnion((await currItem).path)
+        })
+    } catch (e) {
+        Alert.alert(e.message)
+    }
+}
+
+/**
+ * Returns item data for given ID, or null if error occurs
+ * @param item Firebase ID of desired item, assumed to be "items/..."
+ */
+export async function getItem(item) {
+    const itemID = item.substring(6);
+    let itemData;
+    
+    try {
+        itemData = itemRef.doc(itemID).get().data();
+    } catch (e) {
+        console.error("getItem: Error in getting item with ID: ", itemID);
+        alert(e.message);
+        return null;
+    }
+
+    return itemData;
+}
+
+/**
+ * Creates a new Space in Firebase
+ * @param currentUser User requesting creation, will be the owner
+ * @param spaceName Name of the new space
+ * @param spaceType Type of the new space
+ */
 export async function createSpaces(currentUser, spaceName, spaceType) {
     try {
         const currSpace = spaceRef.add({
@@ -88,6 +152,12 @@ export async function createSpaces(currentUser, spaceName, spaceType) {
     }
 }
 
+/**
+ * Deletes the Space ONLY IF currentUser is the owner of the Space
+ * @param currentUser User requesting deletion
+ * @param currentSpace Space targeted for deletion
+ * @returns 
+ */
 export async function deleteSpace(currentUser, currentSpace) {
     try {
         // Authenticate owner is requesting deletion.
@@ -124,10 +194,32 @@ export async function deleteSpace(currentUser, currentSpace) {
     }
 }
 
+/**
+ * Returns space data for given ID, or null if error occurs
+ * @param space Firebase ID of desired space, assumed to be "spaces/..."
+ */
+ export async function getSpace(space) {
+    const spaceID = space.substring(7);
+    let spaceData;
+    
+    try {
+        spaceData = spaceRef.doc(spaceID).get().data();
+    } catch (e) {
+        console.error("getSpace: Error in getting space with ID: ", spaceID);
+        alert(e.message);
+        return null;
+    }
+
+    return spaceData;
+}
+
+/**
+ * Creates a new list reference in Firebase
+ * @param currentSpaceID Space to own the newly created list
+ * @param listName Name of the list
+ */
 export async function createNewList(currentSpaceID, listName) {
     try {
-        console.log("Creating new list!")
-
         const newList = listRef.add({
             name: listName,
             spaceID: currentSpaceID,
@@ -143,6 +235,12 @@ export async function createNewList(currentSpaceID, listName) {
     }
 }
 
+/**
+ * Deletes List reference and transfers all items to corresponding space
+ * @param currentList List to delete
+ * @param currentSpace Space to move all list items to, must own currentList
+ * @returns 
+ */
 export async function deleteList(currentList, currentSpace) {
     try {
         // Verify given list to delete belongs to corresponding space.
@@ -166,9 +264,6 @@ export async function deleteList(currentList, currentSpace) {
             console.log("deleteList: List must belong to corresponding Space");
             return;
         }
-
-        // NOTE: Deleting a list should move items to the unnamed list
-        // A.K.A Transfer all items from list's "items" to space's "items"
         
         // Add items to space's "items"
         const listItems = listRef.doc(listID).items;
@@ -185,6 +280,28 @@ export async function deleteList(currentList, currentSpace) {
     }
 }
 
+/**
+ * Returns list data for given ID, or null if error occurs
+ * @param list Firebase ID of desired list, assumed to be "lists/..."
+ */
+ export async function getList(list) {
+    const listID = list.substring(6);
+    let listData;
+    
+    try {
+        listData = listRef.doc(listID).get().data();
+    } catch (e) {
+        console.error("getSpace: Error in getting space with ID: ", listID);
+        alert(e.message);
+        return null;
+    }
+
+    return listData;
+}
+
+/** 
+ * Authentication Functions
+*/
 export async function signUp(lastName, firstName, email, phone, password, confirmPassword) {
     if (!lastName) {
         alert('First name is required');

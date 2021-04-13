2a60c20e139dc005e4618b8303563abc67055a0b
mgomez <mgomez46@gatech.edu>
2021-04-13 18:50:12 -0400

    more updates to EditSpace

--- screens/EditSpace.js
+++ screens/EditSpace.js
@@ -1,9 +1,11 @@
 import React, { useState, Component, useEffect } from 'react';
 import { ScrollView, StyleSheet, Text, View, SafeAreaView, Modal} from 'react-native';
 import Button from '../components/Button';
+import User from '../components/User';
 import FormInput from '../components/FormInput';
 import { Icon } from 'react-native-elements';
-import { changeSpaceOwner, deleteSpace, getSpace, leaveSpace, updateSpace, getAllMembers } from '../utils/firebaseMethod';
+import { changeSpaceOwner, deleteSpace, getSpace, leaveSpace, updateSpace } from '../utils/firebaseMethod';
+import { db } from '../config/keys';
 
 export default function editSpace({route, navigation}) {
     //route params: spaceID, currUser
@@ -11,26 +13,55 @@ export default function editSpace({route, navigation}) {
     const [modalVisible, setModalVisible] = useState(false)
     const [isOwner, setIsOwner] = useState(false);
     const [members, setMembers] = useState([]);
-    const toggleShared = () => setShared(previousState => ! previousState);
     const currentUser = route.params.currUser;
     const currentSpaceId = route.params.spaceID;
 
+    // db refs
+    const userRef = db.collection('users');
 
-    // Conditional rendering for the change ownership button
-    // User ID must be equal to the owner ID
+
+    // Get the owner of a space
+    //TODO HAS ERRORS??? UNSURE WHY
     useEffect(() => {
         async function getOwner() {
             let spaceOwner = (await getSpace(currentSpaceId)).owner;
             let user = currentUser.uid
+            // compare to see if current user is the space owner
+            if (spaceOwner === user ){
+                console.log('hello')
+            }
             (spaceOwner === user) ? setIsOwner(true) : setIsOwner(false);
         }
        getOwner();
     }, []);
 
+    //grab the potential new owners (exclude current owner)
+    const nonOwners = [];
+    const changeOwner = () => {
+        for (var i = 0; i < members.length; i++){
+            if (!members[i].owner){
+                nonOwners.push(members[i]);
+            }
+        }
+    }
+
     // get the members of a space
     useEffect(() => {
         async function getMembers() {
-            let users = (await getSpace(currentSpaceId)).user;
+            // get the array of users
+            let userIDs = (await getSpace(currentSpaceId)).user;
+            let owner = (await getSpace(currentSpaceId)).owner;
+
+            let users = []
+            for (var i = 0; i < userIDs.length; i++) {
+                let userData = (await userRef.doc(userIDs[i]).get()).data();
+                let initials = userData.firstname.substring(0, 1) + userData.lastname.substring(0, 1);
+                if (userIDs[i] === owner)
+                    users.push({firstName: userData.firstname, lastName: userData.lastname, initials: initials, owner: true})
+                else {
+                    users.push({firstName: userData.firstname, lastName: userData.lastname, initials: initials, owner: false})
+                }
+            }
             setMembers(users);
         }
         getMembers();
@@ -55,35 +86,61 @@ export default function editSpace({route, navigation}) {
                         autoCorrect={false}
                     />
                 <Text style={styles.subtext}>Current Members</Text>
-                
+                <View>
+                    {members.map((user) =>
+                        { return (
+                            <View style={[styles.users, {justifyContent:'space-between'}]}>
+                                <View style={styles.users}>
+                                    <User
+                                        initials={user.initials}
+                                    />
+                                    <Text style={styles.userText}>{user.firstName} {user.lastName} </Text>
+                                </View>
+                                <View style={{paddingRight: 12, paddingTop:16}}>
+                                 {user.owner ? <Icon  name='star' size={36}/> : null}
+                                </View>
+                            </View>
+                    )})}
+                </View>
                 <Text style={[styles.subtext, {paddingVertical: 12}]}>Add Members</Text>
                 <Button
                     name="Generate Code"
                 />
             </View>
             <View style={{marginBottom: 50, width: '100%', position: 'absolute', bottom: 0,}}>
-                <View style={{paddingVertical: 12}}>
+                <Button
+                    name="Update Space"
+                    color='#184254'
+                    onClick={() => { 
+                        console.log(currentSpaceId.substring(7))
+                        updateSpace(currentSpaceId, name);
+                        navigation.navigate('SpacePage') 
+                    }}
+                />
+                {isOwner ? 
+                <View>
+                    <View style={{paddingVertical: 8}}>
+                        <Button
+                            name="Change Owner"
+                            color='#219653'
+                            onClick={() => {
+                                changeOwner();
+                                setModalVisible(true)
+                                // TODO: Change null to new owner id through modal
+                                // changeSpaceOwner(currentUser, null, currentSpaceId);
+                            }}
+                        /> 
+                    </View>
                     <Button
-                        name="Update Space"
-                        color='#184254'
-                        onClick={() => { 
-                            console.log(currentSpaceId.substring(7))
-                            updateSpace(currentSpaceId, name);
-                            navigation.navigate('SpacePage') 
+                        name="Delete Space"
+                        color='#EB5757'
+                        onClick={() => {
+                            deleteSpace(currentUser, currentSpaceId);
+                            navigation.navigate('MySpacesPage')
                         }}
                     />
                 </View>
-                {isOwner ? 
-                <Button
-                    name="Change Owner"
-                    color='#219653'
-                    onClick={() => {
-                        setModalVisible(true)
-                        // TODO: Change null to new owner id through modal
-                        // changeSpaceOwner(currentUser, null, currentSpaceId);
-                    }}
-                /> : null
-                }
+                :                
                 <View style={{paddingVertical: 12}}>
                     <Button
                         name="Leave Space"
@@ -93,15 +150,8 @@ export default function editSpace({route, navigation}) {
                             navigation.navigate('MySpacesPage') 
                         }}
                     />
-                </View>
-                <Button
-                    name="Delete Space"
-                    color='#EB5757'
-                    onClick={() => {
-                        deleteSpace(currentUser, currentSpaceId);
-                        navigation.navigate('MySpacesPage')
-                    }}
-                />
+                 </View> 
+                }
             </View>
         <View style={styles.centeredView}>
             <Modal
@@ -116,6 +166,13 @@ export default function editSpace({route, navigation}) {
                 <View style={styles.centeredView}>
                 <View style={styles.modalView}>
                     <Text style={styles.modalText}>Change Owner</Text>
+                    <View>
+                        {nonOwners.map((user) => {
+                            return (
+                                <Text>{user.firstName}</Text>
+                            )
+                        })}
+                    </View>
                     <Button
                         name="Update Owner"
                         color= "#184254"
@@ -180,6 +237,17 @@ const styles = StyleSheet.create({
         color: '#184254',
         fontSize: 18,
         textAlign: "center"
+      },
+      users: {
+        flexDirection: 'row',
+        alignContent: 'center',
+        paddingVertical: 6,
+      },
+      userText: {
+        fontSize: 18,
+        color: '#184254',
+        paddingLeft: 12,
+        paddingTop: 18 //temp solution, needs to be aligned verticaly
       }
     
 

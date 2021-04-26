de3482cc8f5cabfdd052eb72c2ba451bf3d4603c
Jackson Williams <jwilliams01619@gmail.com>
2021-04-26 00:23:27 -0400

    Ignore logs and warnings for demo

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 import Providers from './navigation';
+
+LogBox.ignoreAllLogs(true)
 
 export default class App extends React.Component {
   render() {
